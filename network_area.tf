ac7a97ecae4740e47a54834e785c36f55f132386
Stanislava Racheva <stanislava.racheva@itgix.com>
2026-09-17 21:45:34 +0300

    provider upgrade

--- network_area.tf
+++ network_area.tf
@@ -6,11 +6,20 @@ locals {
 resource "stackit_network_area" "this" {
   count = var.create_network_area ? 1 : 0
 
-  organization_id  = var.organization_id
-  name             = local.network_area_name
-  transfer_network = var.transfer_network
+  organization_id = var.organization_id
+  name            = local.network_area_name
+}
 
-  network_ranges = [
-    { prefix = var.network_range_prefix }
-  ]
+resource "stackit_network_area_region" "this" {
+  count = var.create_network_area ? 1 : 0
+
+  organization_id = var.organization_id
+  network_area_id = stackit_network_area.this[0].network_area_id
+
+  ipv4 = {
+    transfer_network = var.transfer_network
+    network_ranges = [
+      { prefix = var.network_range_prefix }
+    ]
+  }
 }
